14ac20bcc0fb6600369bcf091607e9726afa8f93
aahmad94 <aka5vm@virginia.edu>
2018-03-29 01:17:52 -0700

    connect fetchWeather action creator to SearchBar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-export default class SearchBar extends Component {
+import React, { Component } from 'react'; 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term: '' };
     this.onInputChange = this.onInputChange.bind(this);
-    this.handleSumbit = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(e) {
@@ -14,6 +18,8 @@ export default class SearchBar extends Component {
   onFormSubmit(e) {
     e.preventDefault();
     // We need to go and fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
@@ -32,3 +38,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
