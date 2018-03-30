08804448c95bdf0163bb6b00bfadc512756a0882
aahmad94 <aka5vm@virginia.edu>
2018-03-29 19:15:06 -0700

    add reducer for weather for incoming fetchWeather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 const API_KEY = "0f801224bb546e79e5135b1c4688ddd4";
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url); 
+
+  console.log('Request:', request);
+
   return {
     type: FETCH_WEATHER,
     payload: request
