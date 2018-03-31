6aa929248055c05424be177eb520b489e4e9be9d
aahmad94 <aka5vm@virginia.edu>
2018-03-31 10:28:58 -0700

    implement google maps api

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class 
 WeatherList extends Component {
@@ -10,9 +11,10 @@ WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp - 273);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lat, lon }  = cityData.city.coord;
     return (
       <tr key={name}>
-        <td>{ name }</td>
+        <td><GoogleMap lat={lat} lon={lon} /></td>
         <td><Chart data={temps} color="blue"  units="C" /></td>
         <td><Chart data={pressures} color="red" units="hPa" /></td>
         <td><Chart data={humidities} color="green" units="%"/></td>
